42d63e7d71f51bcb48b117fec6470f653ea663b7
Vickoman <vicmandlagasca@gmail.com>
2021-01-11 23:26:36 -0500

    Create home and login pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [currentUser, setcurrentUser] = useState<string|null>(null);
+
+  const handleLogin = (user:string) => {
+    setcurrentUser(user)
+  };
+
+  const handleLogout = () => {
+    setcurrentUser(null);
+  }
+  
+  return currentUser ? (
+      <Home />
+    ) : (
+      <Login />
+    );
 }
 
 export default App;
